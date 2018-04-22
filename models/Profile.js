5c821e0308dc929a2002fcf9f2c2b1bc4d30582b
Mihail Zdravkov <michael.zdravkov@gmail.com>
2018-04-22 19:53:10 +0200

    Fixed Profile and ClientProfile models

--- models/Profile.js
+++ models/Profile.js
@@ -18,6 +18,39 @@ const ProfileSchema = new Schema({
         type: Date,
         default: Date.now()
     },
+    lastChanged: {
+        type: Date,
+        required: false
+    },
+    Admins: {
+        type: [mongoose.Types.ObjectId],
+        required: false //TODO: change to true
+    },
+    Contacts: {
+        type: [mongoose.Types.ObjectId],
+        required: false       
+    },
+    Trade: {
+        type: string,
+        required: false
+    },
+    TradeTags: {
+        type: [mongoose.Types.ObjectId],
+        required: false
+    },
+    Needs: {
+        type: string,
+        required: false
+    },
+    NeedsTags: {
+        type: [mongoose.Types.ObjectId],
+        required: false
+    },
+    Areas: {
+        type: [mongoose.Types.ObjectId],
+        required: false
+    }
+
 });
 
 mongoose.model('profiles', ProfileSchema);
